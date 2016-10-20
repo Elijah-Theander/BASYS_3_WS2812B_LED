f1d8e710bd4f503bd9c8dc2f9b3de49e32053b5f
Elijah-Theander <elijahtheander@gmail.com>
2016-10-20 10:27:05 -0600

    Changed timing in stimulus
    Testbench was ending too quickly to verify pulses.

--- Test Benches/tb_bitGenerator.v
+++ Test Benches/tb_bitGenerator.v
@@ -27,10 +27,10 @@ module tb_bitGenerator;
 		$dumpfile("bitGen.vcd");
 		$dumpvars(0,tb_bitGenerator);
 			{doGen,clk,reset} = 3'b001; genMode = 2'b00; #10
-			reset = 0; doGen = 1;                        #260 // RET, should be zero, but not for full RET time.
-			genMode = 2'b10;							 #260 // should see a low NZR pulse
-			genMode = 2'b11;							 #260 // should see a High NZR pulse
-			genMode = 2'b01;                             #260 // should be low, this input shouldn't happen naturally.
+			reset = 0; doGen = 1;                        #2600 // RET, should be zero, but not for full RET time.
+			genMode = 2'b10;							 #2600 // should see a low NZR pulse
+			genMode = 2'b11;							 #2600 // should see a High NZR pulse
+			genMode = 2'b01;                             #2600 // should be low, this input shouldn't happen naturally.
 		$finish;
 	end
 	
