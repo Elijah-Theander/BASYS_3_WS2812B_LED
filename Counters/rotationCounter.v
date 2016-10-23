276e3bc9d1dffa67fab4c30989348098f12fe24e
Elijah-Theander <elijahtheander@gmail.com>
2016-10-23 12:17:25 -0600

    Added rotationCounter.v
    Counter keeping track of how many bits have been sent.

---
+++ Counters/rotationCounter.v
@@ -0,0 +1,37 @@
+/*
+Authored by Elijah Theander
+October 23, 2016
+rotationCounter.v
+96 bit counter that increments everytime a count is done
+and a rotation of the register happens.
+*/
+
+module rotationCounter(sendDone,genDone,clk,reset);
+
+	output sendDone;	//output saying the full send is complete
+	input genDone;		//input saying  increment count.
+	input clk,reset;	//synchronous clock and reset.
+	
+	reg [6:0] S,nS;
+	
+	always @(posedge clk)begin
+		if(reset)begin
+			S <= 7'd0;
+		end
+		else begin
+			S <= nS;
+		end
+	end
+	
+	always @(S,genDone)begin
+		if(S < 7'd97)begin
+			nS = genDone? S+1:S;
+		end
+		else begin
+			nS = 0;
+		end
+	end
+	
+	assign sendDone = (S==96);
+		
+endmodule
