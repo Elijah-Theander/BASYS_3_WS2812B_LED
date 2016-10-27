6bed3d15d5f801d432e47759981fc4ed6c7b9927
Elijah-Theander <elijahtheander@gmail.com>
2016-10-27 12:53:16 -0600

    Fixed capitalization error
    loadregister -> loadRegister

--- Test Benches/tb_shiftRegister.v
+++ Test Benches/tb_shiftRegister.v
@@ -30,7 +30,7 @@ module tb_shiftRegister;
 			loadValue = 96'hAAAAAA_BBBBBB_AAAAAA_BBBBBB;
 			{loadRegister,genDone,clk,reset} =4'b1101;		#20
 			reset = 0;						#20
-			loadregister = 0;					#1000 //this should give us all rotates.
+			loadRegister = 0;					#1000 //this should give us all rotates.
 		$finish;
 	end
 	
