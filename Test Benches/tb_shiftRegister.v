f8ad08049c88f30c8833c8fa4e6433b7ea9d6e06
Elijah-Theander <elijahtheander@gmail.com>
2016-10-27 12:48:20 -0600

    Changed Spacing for readability

--- Test Benches/tb_shiftRegister.v
+++ Test Benches/tb_shiftRegister.v
@@ -12,7 +12,7 @@ module tb_shiftRegister;
 
 	wire 		 registerBit;
 	reg  		 loadRegister;
-	reg [95:0]   loadValue;
+	reg [95:0]       loadValue;
 	reg 		 genDone;
 	reg 		 clk,reset;
 	
@@ -29,8 +29,8 @@ module tb_shiftRegister;
 		$dumpvars(0,tb_shiftRegister);
 			loadValue = 96'hAAAAAA_BBBBBB_AAAAAA_BBBBBB;
 			{loadRegister,genDone,clk,reset} =4'b1101;		#20
-			reset = 0;										#20
-			loadregister = 0;								#1000 //this should give us all rotates.
+			reset = 0;						#20
+			loadregister = 0;					#1000 //this should give us all rotates.
 		$finish;
 	end
 	
