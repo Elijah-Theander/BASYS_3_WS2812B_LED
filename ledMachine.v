74c29a52e1ba161e878864a857453f31d6774f2e
Elijah-Theander <elijahtheander@gmail.com>
2016-11-10 21:00:51 -0700

    Finish ledMachine
    Finished authoring ledMachine.

--- ledMachine.v
+++ ledMachine.v
@@ -7,10 +7,13 @@ a full working machine ready for and XDC file
 and implementation into vivado.
 */
 
-module ledMachine(clk,reset);
-	output
-	input clk,reset;
-	
+module ledMachine(bitOut,btnU,btnD,sw,clk,btnC);
+	output bitOut; //going through one of the pmod headers to the LED strip.
+	input clk;
+	input btnC; //Basys 3 center button, linked to reset.
+	input btnU; //Basys 3 top button, linked to changeMode.
+	input btnD; //Basys 3 bottom button, linked to send.
+	input [15:0] sw; //Basys 3 switches, linked to various things
 	//shiftRegister wires
 	wire        tenHzWire;
 	wire [95:0] rbWire;
@@ -20,35 +23,68 @@ module ledMachine(clk,reset);
 	
 	//Control module wire
 	wire        goWire;
+	wire [1:0]  genModeWire;
+	wire        genDoneWire;
+	wire        doGenWire;
+	wire        sendDoneWire;
+	wire        doRetWire;
+	wire        retDoneWire;
 	
-	tenHzDiv tenHz_Supply(        .tenHz(tenHzWire),
-	                              .enable(1'b1),
-						          .clk(clk), .reset(reset));
+	//PlaceHolder wire.
+	wire [1:0]  placeholder;
+	assign placeholder = sw[3:2];
+	
+	tenHzDiv tenHz_Supply(          .tenHz(tenHzWire),
+	                                .enable(1'b1),
+						            .clk(clk), .reset(btnC));
 						  
-	weeWoo rb_Supply(             .rbSwap(rbWire),
-                                  .tenHzIn(tenHzWire),
-                                  .clk(clk), .reset(reset));
+	weeWoo rb_Supply(               .rbSwap(rbWire),
+                                    .tenHzIn(tenHzWire),
+                                    .clk(clk), .reset(btnC));
 	
-	rainbowReg rainbow_Supply(    .colorCycle(rainbowWire),
-	                              .tenHzin(tenHzWire),
-							      .clk(clk), .reset(reset));
+	rainbowReg rainbow_Supply(      .colorCycle(rainbowWire),
+	                                .tenHzin(tenHzWire),
+							        .clk(clk), .reset(btnC));
 							
-	modeMachine mode_Supply(      .go(goWire),
-                                  .regVal(loadValWire),
-                                  .changeMode(),
-                                  .modeSet(),
-                                  .send(),
-                                  .Green(),
-                                  .Red(),
-                                  .Blue(),
-                                  .rbSwap(rbWire),
-                                  .colorCycle(rainbowWire),
-                                  .clk(clk), .reset(reset));								  
+	modeMachine mode_Supply(        .go(goWire),
+                                    .regVal(loadValWire),
+                                    .changeMode(btnU),
+                                    .modeSet(sw[1:0]),
+                                    .send(btnD),
+                                    .Green(sw[11:8]),
+                                    .Red(sw[15:12]),
+                                    .Blue(sw[7:4]),
+                                    .rbSwap(rbWire),
+                                    .colorCycle(rainbowWire),
+                                    .clk(clk), .reset(btnC));								  
 						  
-	shiftRegister register_Supply(.registerBit(.registerBitWire),
-	                              .loadRegister(loadRegWire),
-								  .loadValue(loadValWire),
-								  .genDone(),
-								  .clk(clk),
-								  .reset(reset));
-endmodule;
+	shiftRegister register_Supply(  .registerBit(.registerBitWire),
+	                                .loadRegister(loadRegWire),
+								    .loadValue(loadValWire),
+								    .genDone(genDoneWire),
+								    .clk(clk), .reset(btnC));
+								  
+	ledControl foreman(             .doGen(doGenWire),
+	                                .doRet(doRetWire),
+								    .loadRegister(loadRegWire),
+								    .genMode(genModeWire),
+								    .go(goWire),
+								    .retDone(retDoneWire),
+								    .sendDone(sendDoneWire)),
+						  		    .registerBit(registerBitWire),
+								    .clk(clk), .reset(btnC));
+								  
+	bitGenerator bit_Supply(        .theBit(bitOut),
+                                    .genDone(genDoneWire),
+								    .genMode(genModeWire),
+								    .doGen(doGenWire),
+								    .clk(clk), .reset(btnC));
+	
+	rotationCounter rotation_Supply(.sendDone(sendDoneWire),
+	                                .genDone(genDoneWire),
+									.clk(clk), .reset(btnC));
+						
+	retCounter ret_Supply(          .retDone(retDoneWire),
+	                                .en(doRetWire),
+									.clk(clk), .reset(btnC));
+endmodule
